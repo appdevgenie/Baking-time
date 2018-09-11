9192132d96959fce5467cdd26d6f785fa47862e8
appdevgenie <appdevgenie@gmail.com>
2018-09-11 15:06:04 +0200

    clean-up

--- app/src/main/java/com/appdevgenie/bakingtime/adapters/RecipeStepsListAdapter.java
+++ app/src/main/java/com/appdevgenie/bakingtime/adapters/RecipeStepsListAdapter.java
@@ -1,11 +1,13 @@
 package com.appdevgenie.bakingtime.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.appdevgenie.bakingtime.R;
@@ -18,6 +20,7 @@ public class RecipeStepsListAdapter extends RecyclerView.Adapter<RecipeStepsList
     private Context context;
     private List<Step> steps;
     private StepClickedListener stepClickedListener;
+    private int rowId = -1;
 
     public RecipeStepsListAdapter(Context context, List<Step> steps, StepClickedListener stepClickedListener) {
         this.context = context;
@@ -33,16 +36,30 @@ public class RecipeStepsListAdapter extends RecyclerView.Adapter<RecipeStepsList
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecipeStepsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final RecipeStepsViewHolder holder, int position) {
         Step step = steps.get(holder.getAdapterPosition());
 
-        int pos = holder.getAdapterPosition();
+        final int pos = holder.getAdapterPosition();
         if (pos == 0) {
             holder.tvNumber.setText(R.string.intro_abbr);
         } else {
             holder.tvNumber.setText(String.valueOf(pos));
         }
         holder.tvDescription.setText(step.getShortDescription());
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stepClickedListener.onStepClicked(steps, pos);
+                rowId = pos;
+                notifyDataSetChanged();
+            }
+        });
+        if(rowId == pos){
+            holder.linearLayout.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+        }else{
+            holder.linearLayout.setBackgroundColor(Color.TRANSPARENT);
+        }
 
     }
 
@@ -54,22 +71,17 @@ public class RecipeStepsListAdapter extends RecyclerView.Adapter<RecipeStepsList
         return steps.size();
     }
 
-    class RecipeStepsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class RecipeStepsViewHolder extends RecyclerView.ViewHolder {
         TextView tvNumber;
         TextView tvDescription;
+        LinearLayout linearLayout;
 
         RecipeStepsViewHolder(View itemView) {
             super(itemView);
 
             tvNumber = itemView.findViewById(R.id.tvStepItemNumber);
             tvDescription = itemView.findViewById(R.id.tvStepItemShortDescription);
-
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            stepClickedListener.onStepClicked(steps, getAdapterPosition());
+            linearLayout = itemView.findViewById(R.id.llItemStepsList);
         }
     }
 
